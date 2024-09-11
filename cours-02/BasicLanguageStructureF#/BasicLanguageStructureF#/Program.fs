27b997a87fb07a8f36de053db646484877d117d5
William Flageol <zhevilh@users.noreply.github.com>
2024-09-11 15:22:44 -0400

    Add cours-02.

---
+++ cours-02/BasicLanguageStructureF#/BasicLanguageStructureF#/Program.fs
@@ -0,0 +1,32 @@
+﻿type Value = int
+and Addition = Instruction * Instruction
+and Subtraction = Instruction * Instruction
+and Equality = Instruction * Instruction
+and If = { Condition: Instruction
+           Then: Instruction
+           Else: Instruction option }
+and Instruction = Value of Value
+                  | Addition of Addition
+                  | Subtraction of Subtraction
+                  | Equality of Equality
+                  | If of If
+
+let rec execute instruction =
+    match instruction with
+    | Value v -> v
+    | Addition (x, y) -> (execute x) + (execute y)
+    | Subtraction (x, y) -> (execute x) - (execute y)
+    | Equality (x, y) -> if (execute x) = (execute y) 
+                         then 1
+                         else 0
+    | If i -> if (execute i.Condition) = 1
+              then (execute i.Then)
+              else match i.Else with
+                   | Some e -> execute e
+                   | None -> 0
+
+let program = If { Condition = Equality (Value 0, Value 1)
+                   Then = Addition (Value 2, Value 3)
+                   Else = Subtraction (Value 5, Value 4) |> Some}
+
+printfn "%A" (execute program)
---
+++ cours-02/Playground/Playground/Program.fs
@@ -0,0 +1,31 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let x = 4
+
+let mutable add = fun x y z -> x + y + z
+
+let c = (2, 4)
+let a = fst c
+let b = snd c
+
+// round 2.6 |> int |> add x 4 |> printfn "%A"
+
+// printfn "%A" (add x 4 (int (round 2.6)))
+
+
+
+
+let list = [2; 3; 4]
+
+let rec addList list =
+    match list with
+    | [] -> 0
+    | head::tail -> head + addList tail
+
+printfn "%A" (addList list)
+
+List.reduce (+) list |>
+printfn "%A"
+
+let double x = x + x
+List.map double list |> printfn "%A"
