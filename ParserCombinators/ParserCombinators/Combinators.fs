72c75c1d78e1affded85800f7df9004cef4dea18
William Flageol <zhevilh@users.noreply.github.com>
2024-10-02 13:58:49 -0400

    Ajouts commentaires.

--- ParserCombinators/ParserCombinators/Combinators.fs
+++ ParserCombinators/ParserCombinators/Combinators.fs
@@ -5,13 +5,14 @@ open Streams
 type Result<'a> = Ok of seq<'a> * Stream
                 | Error
 
-// Exécute le prédicat sur le caractère actuel du stream, 
+/// Exécute le prédicat sur le caractère actuel du stream.
 let one predicate stream =
     let element, newStream = next stream
     if predicate element
     then Ok (List.toSeq [element], newStream)
     else Error
 
+/// Combine séquentiellement deux parsers en un seul. Les résulats sont concaténés.
 let bind p1 p2 stream =
     match p1 stream with
     | Ok (result, stream) -> 
@@ -20,7 +21,10 @@ let bind p1 p2 stream =
         | Error -> Error
     | Error -> Error
 
-/// Applies a parser as many times as possible. This cannot return Error.
+/// Opérateur infix pour bind.
+let (>>=) = bind
+
+/// Applique un parser autant de fois que possible. Ne peut pas retourner Error.
 let many p stream =
     let rec doMany acc stream =
         match p stream with
@@ -28,6 +32,5 @@ let many p stream =
         | Error -> Ok (acc, stream)
     doMany [] stream
 
-let (>>=) = bind
-
+/// Vérifie que le caractère actual fait partie de la liste fournie.
 let chars list stream = one (fun c -> List.contains c list) stream
--- ParserCombinators/ParserCombinators/Streams.fs
+++ ParserCombinators/ParserCombinators/Streams.fs
@@ -5,10 +5,12 @@ type Stream = { Source: seq<char>
 
 let stream source = { Source = source; Index = 0}
 
+/// Retourne le caractère actuel du stream.
 let peek stream = 
     let rest = Seq.skip stream.Index stream.Source
     if Seq.isEmpty rest
     then char 0
     else Seq.head rest
 
+/// Retourne le caractère actuel du stream et avance le curseur.
 let next stream = peek stream, { stream with Index = stream.Index + 1 }
